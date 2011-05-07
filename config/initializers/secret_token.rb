4968e935de7cc4a5055ae84d0bc94d45c8daf986
Ysiad Ferreiras <ysiadf@gmail.com>
2011-05-07 05:39:35 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parkfinder::Application.config.secret_token = 'aeda480fca32cc6b8072f415d3fd77c8876daf9a8b45bf76cca78ca8ec2f03e74feda19d5e2ad1f6eb2257be558238e61dd5536fe54aa3007b6474c7edb86180'
